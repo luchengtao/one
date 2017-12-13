f14134312971a90e28b4c967e9b00b28997d1916
zhuihun12345 <zhuihun12345@163>
2017-12-13 11:04:37 +0800

    remove com.nsn*

--- com.nsn.web.lte.ningbo.sys/src/com/nsn/web/lte/ningbo/sys/action/DoXnzbAction.java
+++
@@ -1,8 +0,0 @@
-package com.nsn.web.lte.ningbo.sys.action;
-
-public class DoXnzbAction {
-	public String index(){
-		return "do_xnzb.html";
-	}
-	
-}
--- com.nsn.web.lte.ningbo.sys/src/com/nsn/web/lte/ningbo/sys/action/IndexAction.java
+++
@@ -1,9 +0,0 @@
-package com.nsn.web.lte.ningbo.sys.action;
-
-import com.nsn.web.lte.mvc.ReqContext;
-
-public class IndexAction {
-	public String index(ReqContext rc){
-		return "index.html";
-	}
-}
